b91fc7336a0cab343d0c3fec92ab8bf3e6bfaa90
Kirt Martin <kirt.martin@centroid.com>
2018-02-09 09:54:19 -0500

    DB Software Download code

---
+++ attributes/default.rb
@@ -0,0 +1,13 @@
+default['centroid_oracle_database']['os_user'] = 'oracle'
+default['centroid_oracle_database']['os_user_home_dir'] = '/home'
+default['centroid_oracle_database']['os_shell'] = '/bin/bash'
+default['centroid_oracle_database']['os_group'] = 'dba'
+
+default['centroid_oracle_database']['db_source_url'] = 'http://chef-assets.centroid.com/software/oracle_db/'
+default['centroid_oracle_database']['db_source_filename'] = 'linuxx64_12201_database.zip'
+
+default['centroid_oracle_database']['oracle_home'] = '/home/oracle/app/oracle'
+
+# Default Oracle Base: /home/oracle/app/oracle
+# Default Software Location: /home/oracle/app/oracle/product/12.1.0/dbhome_1
+# Default Inventory: /home/oracle/app/oraInventory
