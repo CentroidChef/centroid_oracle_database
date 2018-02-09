3bda0de6be5930c5199f85d7a4b2795ff17f6d1b
Kirt Martin <kirt.martin@centroid.com>
2018-02-09 09:57:31 -0500

    DB Software Download code

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -6,17 +6,11 @@
 
 require 'spec_helper'
 
-describe 'centroid_oracle_database::default' do
-  context 'When all attributes are default, on an Ubuntu 16.04' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
+describe 'centroid_weblogic::default' do
+  let(:chef_run) do
+    # for a complete list of available platforms and versions see:
+    # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
+    runner = ChefSpec::ServerRunner.new(platform: 'oracle', version: '7.4')
+    runner.converge(described_recipe)
   end
 end
